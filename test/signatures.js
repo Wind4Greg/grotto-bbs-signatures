f5f1438fedcc68c170e6557428da50e8ab79af94
Wind4Greg <gregb@grotto-networking.com>
2023-02-23 12:56:54 -0800

    Updated signatures and verify.

--- test/signatures.js
+++ test/signatures.js
@@ -4,54 +4,64 @@ import { readFile, readdir } from 'fs/promises';
 
 const maxL = 20; // Use when precomputing the generators
 
-// Read all the signature test files into JavaScript objects
-const vectorPath = './test/fixture_data/bls12-381-sha-256/signature/';
-let testFiles = await readdir(vectorPath);
-// console.log(testFiles);
-let testVectors = [];
-for (let fn of testFiles) {
-  let testVector = JSON.parse(await readFile(vectorPath + fn));
-  testVectors.push(testVector);
-  // console.log(testVector);
-}
+const SHA_PATH = './test/fixture_data/bls12-381-sha-256/';
+const SHAKE_PATH = './test/fixture_data/bls12-381-shake-256/';
 
-describe('Signatures', function () {
-  let gens;
-  before(async function () {
-    gens = await prepareGenerators(maxL); // precompute generators
-  })
+for (let hash of ["SHA-256", "SHAKE-256"]) {
+  let path = SHA_PATH;
+  if (hash == "SHAKE-256") {
+    path = SHAKE_PATH;
+  }
 
-  for (let vector of testVectors) {
-    // Create test name
-    let testName = vector.caseName;
-    if (vector.result.valid) {
-      testName += ":valid";
-    } else {
-      testName += ":invalid:" + vector.result.reason;
-    }
+  // Read all the signature test files into JavaScript objects
+  const vectorPath = path + 'signature/';
+  let testFiles = await readdir(vectorPath);
+  // console.log(testFiles);
+  let testVectors = [];
+  for (let fn of testFiles) {
+    let testVector = JSON.parse(await readFile(vectorPath + fn));
+    testVectors.push(testVector);
+    // console.log(testVector);
+  }
 
-    // We only check signature generation for "valid" test signatures
-    if (vector.result.valid) {
-      it("signature:" + testName, async function () {
+  describe('Signatures ' + hash, function () {
+    let gens;
+    before(async function () {
+      gens = await prepareGenerators(maxL, hash); // precompute generators
+    })
+
+    for (let vector of testVectors) {
+      // Create test name
+      let testName = vector.caseName;
+      if (vector.result.valid) {
+        testName += ":valid";
+      } else {
+        testName += ":invalid:" + vector.result.reason;
+      }
+
+      // We only check signature generation for "valid" test signatures
+      if (vector.result.valid) {
+        it("signature " + hash + ": " + testName, async function () {
+          let messagesOctets = vector.messages.map(msg => hexToBytes(msg));
+          let msg_scalars = await messages_to_scalars(messagesOctets, hash);
+          let headerBytes = hexToBytes(vector.header);
+          let secretScalar = BigInt("0x" + vector.signerKeyPair.secretKey);
+          let publicBytes = hexToBytes(vector.signerKeyPair.publicKey)
+          let result = await sign(secretScalar, publicBytes, headerBytes, msg_scalars, gens, hash);
+          assert.equal(bytesToHex(result), vector.signature, 'signatures should match');
+        });
+      }
+      // We verify against all signatures whether valid or invalid
+      it("verify " + hash + ": " + testName, async function () {
         let messagesOctets = vector.messages.map(msg => hexToBytes(msg));
-        let msg_scalars = await messages_to_scalars(messagesOctets);
+        let msg_scalars = await messages_to_scalars(messagesOctets, hash);
+        let gens = await prepareGenerators(vector.messages.length, hash); // Generate enough for all messages
         let headerBytes = hexToBytes(vector.header);
-        let secretScalar = BigInt("0x" + vector.signerKeyPair.secretKey);
         let publicBytes = hexToBytes(vector.signerKeyPair.publicKey)
-        let result = await sign(secretScalar, publicBytes, headerBytes, msg_scalars, gens);
-        assert.equal(bytesToHex(result), vector.signature, 'signatures should match');
+        let signature = hexToBytes(vector.signature);
+        let verified = await verify(publicBytes, signature, headerBytes, msg_scalars, gens, hash);
+        assert.equal(verified, vector.result.valid);
       });
     }
-    // We verify against all signatures whether valid or invalid
-    it("verify:" + testName, async function () {
-      let messagesOctets = vector.messages.map(msg => hexToBytes(msg));
-      let msg_scalars = await messages_to_scalars(messagesOctets);
-      let gens = await prepareGenerators(vector.messages.length); // Generate enough for all messages
-      let headerBytes = hexToBytes(vector.header);
-      let publicBytes = hexToBytes(vector.signerKeyPair.publicKey)
-      let signature = hexToBytes(vector.signature);
-      let verified = await verify(publicBytes, signature, headerBytes, msg_scalars, gens);
-      assert.equal(verified, vector.result.valid);
-    });
-  }
-});
+  });
+}
