e5e0142707fcdfbf44fe02bc730b5c7e972d4693
Wind4Greg <gregb@grotto-networking.com>
2023-02-23 16:12:04 -0800

    Updated proof verification tests. ProofGenSeeded test updated.

--- test/proofGenSeeded.js
+++ test/proofGenSeeded.js
@@ -19,52 +19,62 @@ const testPairs = [
   { proofFile: "proof003.json", signatureFile: "signature004.json" },
 ];
 
-// Read all the proof test files into JavaScript objects
-const sigPath = './test/fixture_data/bls12-381-sha-256/signature/';
-const proofPath = './test/fixture_data/bls12-381-sha-256/proof/';
-// console.log(testFiles);
-let testVectors = [];
-for (let pair of testPairs) {
-  let proofBundle = JSON.parse(await readFile(proofPath + pair.proofFile));
-  let sigBundle = JSON.parse(await readFile(sigPath + pair.signatureFile));
-  testVectors.push({ proofBundle: proofBundle, sigBundle: sigBundle });
-}
-// console.log(testVectors);
+const SHA_PATH = './test/fixture_data/bls12-381-sha-256/';
+const SHAKE_PATH = './test/fixture_data/bls12-381-shake-256/';
 
-describe('Proof Generation Seeded Validation', function () {
-  let gens;
-  before(async function () {
-    gens = await prepareGenerators(maxL); // precompute generators
-  })
-
-  for (let vector of testVectors) {
-    // Create test name
-    let testName = vector.proofBundle.caseName;
-
-
-    it(testName, async function () {
-      let msg_scalars, headerBytes, publicBytes, signature;
-      // Get all the signature related stuff
-      let sigBundle = vector.sigBundle;
-      let messagesOctets = sigBundle.messages.map(msg => hexToBytes(msg));
-      msg_scalars = await messages_to_scalars(messagesOctets);
-      headerBytes = hexToBytes(sigBundle.header);
-      publicBytes = hexToBytes(sigBundle.signerKeyPair.publicKey);
-      signature = hexToBytes(sigBundle.signature);
-
-      // From the test vector get the disclosed indices and messages
-      let proofBundle = vector.proofBundle;
-      let msgsObject = proofBundle.revealedMessages;
-      let disclosedIndexes = [];
-      for (let field in msgsObject) {
-        disclosedIndexes.push(parseInt(field));
-      }
-
-      let ph = hexToBytes(proofBundle.presentationHeader);
-      let proof = await proofGen(publicBytes, signature, headerBytes, ph, msg_scalars, disclosedIndexes,
-         gens, rand_scalar_func);
-      // console.log(bytesToHex(proof));
-      assert.equal(bytesToHex(proof), proofBundle.proof);
-    });
+for (let hashType of ["SHA-256", "SHAKE-256"]) {
+  let path = SHA_PATH;
+  if (hashType == "SHAKE-256") {
+    path = SHAKE_PATH;
   }
-});
+
+  // Read all the proof test files into JavaScript objects
+  const sigPath = path + 'signature/';
+  const proofPath = path + 'proof/';
+  // console.log(testFiles);
+  let testVectors = [];
+  for (let pair of testPairs) {
+    let proofBundle = JSON.parse(await readFile(proofPath + pair.proofFile));
+    let sigBundle = JSON.parse(await readFile(sigPath + pair.signatureFile));
+    testVectors.push({ proofBundle: proofBundle, sigBundle: sigBundle });
+  }
+  // console.log(testVectors);
+
+  describe('Proof Generation Seeded Validation ' + hashType, function () {
+    let gens;
+    before(async function () {
+      gens = await prepareGenerators(maxL, hashType); // precompute generators
+    })
+
+    for (let vector of testVectors) {
+      // Create test name
+      let testName = vector.proofBundle.caseName;
+
+
+      it(testName + " " + hashType, async function () {
+        let msg_scalars, headerBytes, publicBytes, signature;
+        // Get all the signature related stuff
+        let sigBundle = vector.sigBundle;
+        let messagesOctets = sigBundle.messages.map(msg => hexToBytes(msg));
+        msg_scalars = await messages_to_scalars(messagesOctets, hashType);
+        headerBytes = hexToBytes(sigBundle.header);
+        publicBytes = hexToBytes(sigBundle.signerKeyPair.publicKey);
+        signature = hexToBytes(sigBundle.signature);
+
+        // From the test vector get the disclosed indices and messages
+        let proofBundle = vector.proofBundle;
+        let msgsObject = proofBundle.revealedMessages;
+        let disclosedIndexes = [];
+        for (let field in msgsObject) {
+          disclosedIndexes.push(parseInt(field));
+        }
+
+        let ph = hexToBytes(proofBundle.presentationHeader);
+        let proof = await proofGen(publicBytes, signature, headerBytes, ph, msg_scalars, disclosedIndexes,
+          gens, hashType, rand_scalar_func);
+        // console.log(bytesToHex(proof));
+        assert.equal(bytesToHex(proof), proofBundle.proof);
+      });
+    }
+  });
+}
--- test/proofVerification.js
+++ test/proofVerification.js
@@ -7,52 +7,61 @@ import { readFile, readdir } from 'fs/promises';
 
 const maxL = 20; // Use when precomputing the generators
 
-// Read all the proof test files into JavaScript objects
-const vectorPath = './test/fixture_data/bls12-381-sha-256/proof/';
-let testFiles = await readdir(vectorPath);
-// console.log(testFiles);
-let testVectors = [];
-for (let fn of testFiles) {
-  let testVector = JSON.parse(await readFile(vectorPath + fn));
-  testVectors.push(testVector);
-  // console.log(testVector);
-}
+const SHA_PATH = './test/fixture_data/bls12-381-sha-256/';
+const SHAKE_PATH = './test/fixture_data/bls12-381-shake-256/';
 
-describe('Proof Verification', function () {
-  let gens;
-  before(async function () {
-    gens = await prepareGenerators(maxL); // precompute generators
-  })
-
-  for (let vector of testVectors) {
-    // Create test name
-    let testName = vector.caseName;
-    if (vector.result.valid) {
-      testName += ":valid";
-    } else {
-      testName += ":invalid:" + vector.result.reason;
-    }
-
-    it(testName, async function () {
-      // From the test vector get the disclosed indices and messages
-      let msgsObject = vector.revealedMessages;
-      let disclosedIndexes = [];
-      let messagesOctets = [];
-      for (let field in msgsObject) {
-        disclosedIndexes.push(parseInt(field));
-        messagesOctets.push(hexToBytes(msgsObject[field]));
-      }
-      // console.log(disclosedIndexes);
-      // console.log(messagesOctets);
-      let disclosedMsgScalars = await messages_to_scalars(messagesOctets);
-      let headerBytes = hexToBytes(vector.header);
-      let publicBytes = hexToBytes(vector.signerPublicKey);
-      let L = vector.totalMessageCount;
-      let proof = hexToBytes(vector.proof);
-      let ph = hexToBytes(vector.presentationHeader);
-      let result = await proofVerify(publicBytes, proof, L, headerBytes, ph, disclosedMsgScalars, 
-          disclosedIndexes, gens);
-      assert.equal(result, vector.result.valid);
-    });
+for (let hash of ["SHA-256", "SHAKE-256"]) {
+  let path = SHA_PATH;
+  if (hash == "SHAKE-256") {
+    path = SHAKE_PATH;
   }
-});
+  // Read all the proof test files into JavaScript objects
+  const vectorPath = path + 'proof/';
+  let testFiles = await readdir(vectorPath);
+  // console.log(testFiles);
+  let testVectors = [];
+  for (let fn of testFiles) {
+    let testVector = JSON.parse(await readFile(vectorPath + fn));
+    testVectors.push(testVector);
+    // console.log(testVector);
+  }
+
+  describe('Proof Verification ' + hash, function () {
+    let gens;
+    before(async function () {
+      gens = await prepareGenerators(maxL, hash); // precompute generators
+    })
+
+    for (let vector of testVectors) {
+      // Create test name
+      let testName = vector.caseName;
+      if (vector.result.valid) {
+        testName += ":valid";
+      } else {
+        testName += ":invalid:" + vector.result.reason;
+      }
+
+      it(testName + " " + hash, async function () {
+        // From the test vector get the disclosed indices and messages
+        let msgsObject = vector.revealedMessages;
+        let disclosedIndexes = [];
+        let messagesOctets = [];
+        for (let field in msgsObject) {
+          disclosedIndexes.push(parseInt(field));
+          messagesOctets.push(hexToBytes(msgsObject[field]));
+        }
+        // console.log(disclosedIndexes);
+        // console.log(messagesOctets);
+        let disclosedMsgScalars = await messages_to_scalars(messagesOctets, hash);
+        let headerBytes = hexToBytes(vector.header);
+        let publicBytes = hexToBytes(vector.signerPublicKey);
+        let L = vector.totalMessageCount;
+        let proof = hexToBytes(vector.proof);
+        let ph = hexToBytes(vector.presentationHeader);
+        let result = await proofVerify(publicBytes, proof, L, headerBytes, ph, disclosedMsgScalars,
+          disclosedIndexes, gens, hash);
+        assert.equal(result, vector.result.valid);
+      });
+    }
+  });
+}
