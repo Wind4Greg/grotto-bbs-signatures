1549bd82483d0becb745c9a0a6f70ffd32a1c7f8
Wind4Greg <gregb@grotto-networking.com>
2023-02-27 09:56:43 -0800

    Issues with proof verify still.

--- test/hash2scalar.js
+++ test/hash2scalar.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { hexToBytes, hash_to_scalar, os2ip } from '../lib/BBS.js';
+import { hexToBytes, hash_to_scalar, os2ip, bytesToHex } from '../lib/BBS.js';
 import { readFile } from 'fs/promises';
 
 const SHA_PATH = './fixture_data/bls12-381-sha-256/';
@@ -10,38 +10,23 @@ for (let hash of ["SHA-256", "SHAKE-256"]) {
   if (hash == "SHAKE-256") {
     path = SHAKE_PATH;
   }
-  const h2s001 = JSON.parse(
+  const h2s = JSON.parse(
     await readFile(
-      new URL(path + 'h2s/h2s001.json', import.meta.url)
+      new URL(path + 'h2s.json', import.meta.url)
     )
   );
 
-  const h2s002 = JSON.parse(
-    await readFile(
-      new URL(path + 'h2s/h2s002.json', import.meta.url)
-    )
-  );
 
   describe('Hash to Scalar ' + hash, function () {
-    it(h2s001.caseName, async function () {
-      // console.log(h2s001);
-      let msg_octets = hexToBytes(h2s001.message);
-      let dst = hexToBytes(h2s001.dst);
-      let count = h2s001.count;
-      let result = await hash_to_scalar(msg_octets, count, dst, hash);
-      let expected = os2ip(hexToBytes(h2s001.scalars[0]));
+    it(h2s.caseName, async function () {
+      // console.log(h2s);
+      let msg_octets = hexToBytes(h2s.message);
+      let dst = hexToBytes(h2s.dst);
+      let result = await hash_to_scalar(msg_octets, dst, hash);
+      // console.log("Computed scalar:");
+      // console.log(result.toString(16));
+      let expected = os2ip(hexToBytes(h2s.scalar));
       assert.equal(result, expected);
-    });
-    it(h2s002.caseName, async function () {
-      // console.log(h2s002);
-      let msg_octets = hexToBytes(h2s002.message);
-      let dst = hexToBytes(h2s002.dst);
-      let count = h2s002.count;
-      let result = await hash_to_scalar(msg_octets, count, dst, hash);
-      for (let i = 0; i < count; i++) {
-        let expected = os2ip(hexToBytes(h2s002.scalars[i]));
-        assert.equal(result[i], expected);
-      }
     });
   });
 }
--- test/proofVerification.js
+++ test/proofVerification.js
@@ -10,9 +10,9 @@ const maxL = 20; // Use when precomputing the generators
 const SHA_PATH = './test/fixture_data/bls12-381-sha-256/';
 const SHAKE_PATH = './test/fixture_data/bls12-381-shake-256/';
 
-for (let hash of ["SHA-256", "SHAKE-256"]) {
+for (let hashType of ["SHA-256", "SHAKE-256"]) {
   let path = SHA_PATH;
-  if (hash == "SHAKE-256") {
+  if (hashType == "SHAKE-256") {
     path = SHAKE_PATH;
   }
   // Read all the proof test files into JavaScript objects
@@ -26,10 +26,10 @@ for (let hash of ["SHA-256", "SHAKE-256"]) {
     // console.log(testVector);
   }
 
-  describe('Proof Verification ' + hash, function () {
+  describe('Proof Verification ' + hashType, function () {
     let gens;
     before(async function () {
-      gens = await prepareGenerators(maxL, hash); // precompute generators
+      gens = await prepareGenerators(maxL, hashType); // precompute generators
     })
 
     for (let vector of testVectors) {
@@ -41,7 +41,7 @@ for (let hash of ["SHA-256", "SHAKE-256"]) {
         testName += ":invalid:" + vector.result.reason;
       }
 
-      it(testName + " " + hash, async function () {
+      it(testName + " " + hashType, async function () {
         // From the test vector get the disclosed indices and messages
         let msgsObject = vector.revealedMessages;
         let disclosedIndexes = [];
@@ -52,14 +52,14 @@ for (let hash of ["SHA-256", "SHAKE-256"]) {
         }
         // console.log(disclosedIndexes);
         // console.log(messagesOctets);
-        let disclosedMsgScalars = await messages_to_scalars(messagesOctets, hash);
+        let disclosedMsgScalars = await messages_to_scalars(messagesOctets, hashType);
         let headerBytes = hexToBytes(vector.header);
         let publicBytes = hexToBytes(vector.signerPublicKey);
         let L = vector.totalMessageCount;
         let proof = hexToBytes(vector.proof);
         let ph = hexToBytes(vector.presentationHeader);
         let result = await proofVerify(publicBytes, proof, L, headerBytes, ph, disclosedMsgScalars,
-          disclosedIndexes, gens, hash);
+          disclosedIndexes, gens, hashType);
         assert.equal(result, vector.result.valid);
       });
     }
