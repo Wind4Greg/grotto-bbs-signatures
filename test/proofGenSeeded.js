bd31b044f9dece80ae84f533cdc86eeb4058a596
Wind4Greg <gregb@grotto-networking.com>
2023-11-19 11:58:22 -0800

    Clean up proof gen seeded tests. Includes all valid test cases. Clean up proof gen code a bit.

--- test/proofGenSeeded.js
+++ test/proofGenSeeded.js
@@ -12,11 +12,8 @@ import {readFile} from 'fs/promises';
 const maxL = 20; // Use when precomputing the generators
 
 // Need the signatures that go with the proofs.
-const testPairs = [
-  {proofFile: 'proof001.json', signatureFile: 'signature001.json'},
-  {proofFile: 'proof002.json', signatureFile: 'signature004.json'},
-  {proofFile: 'proof003.json', signatureFile: 'signature004.json'},
-];
+const testFiles = ['proof001.json', 'proof002.json', 'proof003.json',
+  'proof014.json', 'proof015.json'];
 
 const SHA_PATH = './test/fixture_data/bls12-381-sha-256/';
 const SHAKE_PATH = './test/fixture_data/bls12-381-shake-256/';
@@ -30,14 +27,12 @@ for(const hashType of ['SHA-256', 'SHAKE-256']) {
   const seed = hexToBytes('332e313431353932363533353839373933323338343632363433333833323739');
   const rand_scalar_func = seeded_random_scalars.bind(null, seed, hashType);
   // Read all the proof test files into JavaScript objects
-  const sigPath = path + 'signature/';
   const proofPath = path + 'proof/';
   // console.log(testFiles);
   const testVectors = [];
-  for(const pair of testPairs) {
-    const proofBundle = JSON.parse(await readFile(proofPath + pair.proofFile));
-    const sigBundle = JSON.parse(await readFile(sigPath + pair.signatureFile));
-    testVectors.push({proofBundle, sigBundle});
+  for(const filename of testFiles) {
+    const proofBundle = JSON.parse(await readFile(proofPath + filename));
+    testVectors.push(proofBundle);
   }
   // console.log(testVectors);
 
@@ -47,34 +42,30 @@ for(const hashType of ['SHA-256', 'SHAKE-256']) {
       gens = await prepareGenerators(maxL, hashType); // precompute generators
     });
 
-    for(const vector of testVectors) {
+    for(const proofBundle of testVectors) {
       // Create test name
-      const testName = vector.proofBundle.caseName;
+      const testName = proofBundle.caseName;
 
       it(testName + ' ' + hashType, async function() {
         // Get all the signature related stuff
-        const sigBundle = vector.sigBundle;
-        const messagesOctets = sigBundle.messages.map(msg => hexToBytes(msg));
+        const messagesOctets = proofBundle.messages.map(msg => hexToBytes(msg));
         const msg_scalars = await messages_to_scalars(messagesOctets, hashType);
-        const headerBytes = hexToBytes(sigBundle.header);
-        const publicBytes = hexToBytes(sigBundle.signerKeyPair.publicKey);
-        const signature = hexToBytes(sigBundle.signature);
+        const headerBytes = hexToBytes(proofBundle.header);
+        const publicBytes = hexToBytes(proofBundle.signerPublicKey);
+        const signature = hexToBytes(proofBundle.signature);
 
         // From the test vector get the disclosed indices and messages
-        const proofBundle = vector.proofBundle;
+
         const disclosedIndexes = proofBundle.disclosedIndexes;
-        // for(const field in msgsObject) {
-        //   disclosedIndexes.push(parseInt(field));
-        // }
 
         const ph = hexToBytes(proofBundle.presentationHeader);
         const proof = await proofGen(publicBytes, signature, headerBytes, ph,
           msg_scalars, disclosedIndexes, gens, hashType, rand_scalar_func);
-        console.log("Computed Proof:");
-        console.log(bytesToHex(proof));
-        console.log("Test vector Proof:");
-        console.log(proofBundle.proof);
-        console.log(`is equal?: ${bytesToHex(proof) === proofBundle.proof}`);
+        // console.log("Computed Proof:");
+        // console.log(bytesToHex(proof));
+        // console.log("Test vector Proof:");
+        // console.log(proofBundle.proof);
+        // console.log(`is equal?: ${bytesToHex(proof) === proofBundle.proof}`);
         // console.log('Computed proof raw values:');
         // console.log(octets_to_proof(proof));
         // console.log('Test Vector proof raw values:');
