e761b3100a72bff5f1f64bcff57bcde12d9015fc
Wind4Greg <gregb@grotto-networking.com>
2024-03-13 11:52:01 -0700

    Pseudonym sign, verify, with tests and test vector.

---
+++ pseudonym_test/verifyTest.js
@@ -0,0 +1,47 @@
+/* global describe, it */
+/* eslint-disable max-len */
+import {API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE, hexToBytes} from '../lib/BBS.js';
+import {readdir, readFile} from 'fs/promises';
+import {assert} from 'chai';
+import {PseudonymVerify} from '../lib/PseudonymBBS.js';
+import {bytesToHex} from '@noble/hashes/utils';
+
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const SHA_PATH = __dirname + '/fixture_data/bls12-381-sha-256/signature/';
+const SHAKE_PATH = __dirname + '/fixture_data/bls12-381-shake-256/signature/';
+const message_file = __dirname + '/fixture_data/messages.json';
+const pid_vid_file = __dirname + '/fixture_data/pid_vid.json';
+
+const messages = (JSON.parse(await readFile(message_file))).map(m_hex => hexToBytes(m_hex));
+// console.log('messages:');
+// console.log(messages.map(m => bytesToHex(m)));
+const pid = hexToBytes(JSON.parse(await readFile(pid_vid_file)).pid);
+// console.log(`pid: ${bytesToHex(pid)}`);
+for(const api_id of [API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE]) {
+  let path = SHA_PATH;
+  if(api_id.includes('SHAKE-256')) {
+    path = SHAKE_PATH;
+  }
+  const files = await readdir(path);
+  // get all the test vectors in the dir
+  const testVectors = [];
+  for(const fn of files) {
+    testVectors.push(JSON.parse(await readFile(path + fn)));
+  }
+
+  describe('Pseudonym Signature verification for ' + api_id, async function() {
+    for(let i = 0; i < testVectors.length; i++) { // testVectors.length
+      const sigFixture = testVectors[i];
+      it(`case: ${sigFixture.caseName}`, async function() {
+        const PK = hexToBytes(sigFixture.signerKeyPair.publicKey);
+        const header = hexToBytes(sigFixture.header);
+        const signature = hexToBytes(sigFixture.signature);
+        const result = await PseudonymVerify(PK, signature, header, messages, pid, api_id);
+        assert.isTrue(result);
+      });
+    }
+  });
+}
