78c07cd0f0a07bfe2ca94d6d3e6198a83d53a1fc
Wind4Greg <gregb@grotto-networking.com>
2024-03-16 08:14:10 -0700

    Started hidden pid pseudonyms.  Commit, sign, and verify tests.

---
+++ pseudonym_test/hiddenPidVerifyTest.js
@@ -0,0 +1,47 @@
+/* global describe, it */
+/* eslint-disable max-len */
+import {API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE, hexToBytes} from '../lib/BBS.js';
+import {readdir, readFile} from 'fs/promises';
+import {assert} from 'chai';
+import {BlindVerify} from '../lib/BlindBBS.js';
+import {bytesToHex} from '@noble/hashes/utils';
+
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const SHA_PATH = __dirname + '/fixture_data/bls12-381-sha-256/hiddenPidSignature/';
+const SHAKE_PATH = __dirname + '/fixture_data/bls12-381-shake-256/hiddenPidSignature/';
+const message_file = __dirname + '/fixture_data/messages.json';
+
+const messages = (JSON.parse(await readFile(message_file))).map(m_hex => hexToBytes(m_hex));
+// console.log('messages:');
+// console.log(messages.map(m => bytesToHex(m)));
+for(const api_id of [API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE]) { // , API_ID_PSEUDONYM_BBS_SHAKE
+  let path = SHA_PATH;
+  if(api_id.includes('SHAKE-256')) {
+    path = SHAKE_PATH;
+  }
+  const files = await readdir(path);
+  // get all the test vectors in the dir
+  const testVectors = [];
+  for(const fn of files) {
+    testVectors.push(JSON.parse(await readFile(path + fn)));
+  }
+
+  describe('Hidden pid Pseudonym Signature generation for ' + api_id, async function() {
+    for(let i = 0; i < testVectors.length; i++) { // testVectors.length
+      const sigFixture = testVectors[i];
+      it(`case: ${sigFixture.caseName}`, async function() {
+        const pid = hexToBytes(sigFixture.pid);
+        const proverBlind = BigInt('0x' + sigFixture.proverBlind);
+        const PK = hexToBytes(sigFixture.signerKeyPair.publicKey);
+        const header = hexToBytes(sigFixture.header);
+        const signature = hexToBytes(sigFixture.signature);
+        const result = await BlindVerify(PK, signature, header, messages, [pid],
+          proverBlind, 0n, api_id);
+        assert.isTrue(result);
+      });
+    }
+  });
+}
---
+++ pseudonym_test/pidCommitTest.js
@@ -0,0 +1,47 @@
+/* global describe, URL, it, before, TextEncoder*/
+/* eslint-disable max-len */
+import {API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE, hexToBytes,
+  seeded_random_scalars} from '../lib/BBS.js';
+import {calcM, commit} from '../lib/BlindBBS.js';
+import {readdir, readFile} from 'fs/promises';
+import {assert} from 'chai';
+import {bytesToHex} from '@noble/hashes/utils';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const SHA_PATH = __dirname + '/fixture_data/bls12-381-sha-256/pid_commit/';
+const SHAKE_PATH = __dirname + '/fixture_data/bls12-381-shake-256/pid_commit/';
+
+for(const api_id of [API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE]) {
+  let path = SHA_PATH;
+  if(api_id.includes('SHAKE-256')) {
+    path = SHAKE_PATH;
+  }
+  const files = await readdir(path);
+  // get all the test vectors in the dir
+  const testVectors = [];
+  for(const fn of files) {
+    testVectors.push(JSON.parse(await readFile(path + fn)));
+  }
+
+  describe('Pid commit generation for ' + api_id, async function() {
+    for(const commitFixture of testVectors) {
+      it(`case: ${commitFixture.caseName}`, async function() {
+        const msgs_in_octets = commitFixture.committedMessages.map(hexMsg =>
+          hexToBytes(hexMsg));
+        const seed = new TextEncoder().encode(commitFixture.mockRngParameters.SEED);
+        const rng_dst = commitFixture.mockRngParameters.commit.DST;
+        const rand_scalar_func = seeded_random_scalars.bind(null, seed, rng_dst);
+        const [commit_with_proof_octs, secret_prover_blind] =
+            await commit(msgs_in_octets, api_id, rand_scalar_func);
+        console.log(`commit with proof (hex): ${bytesToHex(commit_with_proof_octs)}`);
+        console.log(`secret prover blind (hex): ${secret_prover_blind.toString(16)}`);
+        console.log(`calcM: ${calcM(commit_with_proof_octs)}`);
+        assert.equal(bytesToHex(commit_with_proof_octs),
+          commitFixture.commitmentWithProof);
+      });
+    }
+  });
+}
