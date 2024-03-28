00b63a1725cc797e7c651a8330d9699d49ac17c7
Wind4Greg <gregb@grotto-networking.com>
2024-03-27 19:15:46 -0700

    Fixed errors in validate commitments and related tests.

--- blind_test/validateCommitTest.js
+++ blind_test/validateCommitTest.js
@@ -14,7 +14,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const SHA_PATH = __dirname + '/fixture_data/bls12-381-sha-256/commit/';
 const SHAKE_PATH = __dirname + '/fixture_data/bls12-381-shake-256/commit/';
 
-for(const api_id of [API_ID_BLIND_BBS_SHA, API_ID_BLIND_BBS_SHAKE]) {
+for(const api_id of [API_ID_BLIND_BBS_SHA, API_ID_BLIND_BBS_SHAKE]) { // , API_ID_BLIND_BBS_SHAKE
   let path = SHA_PATH;
   if(api_id.includes('SHAKE-256')) {
     path = SHAKE_PATH;
@@ -34,6 +34,7 @@ for(const api_id of [API_ID_BLIND_BBS_SHA, API_ID_BLIND_BBS_SHAKE]) {
         const [commit, M] =
             await deserialize_and_validate_commit(commitmentWithProof, gens, api_id);
         assert.isTrue(commitFixture.commitmentWithProof.startsWith(bytesToHex(commit.toRawBytes(true))));
+        // console.log(`M = ${M}`);
       });
     }
   });
