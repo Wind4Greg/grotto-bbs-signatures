afedd79a953c16d9afea64891fd751309b8e0223
Wind4Greg <gregb@grotto-networking.com>
2025-04-25 13:58:57 -0700

    Updated CommitWithNym to take a vector of prover nyms. Updated all test vectors to use an array and added tests with an array of 10 prover_nyms.

--- pseudonym_test/nymCommitTest.js
+++ pseudonym_test/nymCommitTest.js
@@ -17,7 +17,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const SHA_PATH = __dirname + '/fixture_data/bls12-381-sha-256/nymCommit/';
 const SHAKE_PATH = __dirname + '/fixture_data/bls12-381-shake-256/nymCommit/';
 
-for(const api_id of [API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE]) { //API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE
+for(const api_id of [API_ID_PSEUDONYM_BBS_SHAKE]) { //API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE
   let path = SHA_PATH;
   if(api_id.includes('SHAKE-256')) {
     path = SHAKE_PATH;
@@ -26,9 +26,12 @@ for(const api_id of [API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE]) { //
   // get all the test vectors in the dir
   const testVectors = [];
   for(const fn of files) {
-    let vectorObj = JSON.parse(await readFile(path + fn));
+    const vectorObj = JSON.parse(await readFile(path + fn));
     vectorObj.filename = fn;
-    testVectors.push(vectorObj);
+    if(fn == "nymCommit004.json") {
+      testVectors.push(vectorObj);
+    }
+
   }
 
   describe('Prover Nym commit generation for ' + api_id, async function () {
@@ -36,14 +39,17 @@ for(const api_id of [API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE]) { //
       it(`file: ${commitFixture.filename}, case: ${commitFixture.caseName}`, async function () {
         const msgs_in_octets = commitFixture.committedMessages.map(hexMsg =>
           hexToBytes(hexMsg));
-        const prover_nym = BigInt('0x' + commitFixture.proverNym);
+        const prover_nyms = [];
+        for(const proverNym of commitFixture.proverNyms) {
+          prover_nyms.push(BigInt('0x' + proverNym));
+        }
         const prover_blind = commitFixture.proverBlind;
         const seed = new TextEncoder().encode(commitFixture.mockRngParameters.SEED);
         const rng_dst = commitFixture.mockRngParameters.commit.DST;
         const rand_scalar_func = seeded_random_scalars.bind(null, seed, rng_dst);
         // CommitWithNym(messages, prover_nym, api_id, and_scalars = calculate_random_scalars)
         const [commit_with_proof_octs, secret_prover_blind] =
-          await CommitWithNym(msgs_in_octets, prover_nym,api_id, rand_scalar_func);
+          await CommitWithNym(msgs_in_octets, prover_nyms, api_id, rand_scalar_func);
         // console.log(`commit with proof (hex): ${bytesToHex(commit_with_proof_octs)}`);
         // console.log(`secret prover blind (hex): ${secret_prover_blind.toString(16)}`);
         assert.equal(bytesToHex(commit_with_proof_octs),
