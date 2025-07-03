bbe55bb636e3f2ea8012989260cc6feec73687d7
Wind4Greg <gregb@grotto-networking.com>
2025-07-03 09:27:06 -0700

    Add N=10 proof gen/verify test vectors for SHAKE.

--- pseudonym_test/nymProofGenTest.js
+++ pseudonym_test/nymProofGenTest.js
@@ -23,7 +23,7 @@ const SHAKE_PATH = __dirname + "/fixture_data/bls12-381-shake-256/nymProof/";
 const SUPP_PATH = __dirname + '/fixture_data/bls12-381-sha-256/supplement/';
 await mkdir(SUPP_PATH, { recursive: true });
 
-for(const api_id of [API_ID_PSEUDONYM_BBS_SHA]) {
+for(const api_id of [API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE]) {
   // API_ID_PSEUDONYM_BBS_SHA, API_ID_PSEUDONYM_BBS_SHAKE
   let path = SHA_PATH;
   if (api_id.includes("SHAKE-256")) {
